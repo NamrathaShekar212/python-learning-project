34f0db1df9988f1fb2df14d4a04ca926afbb4cf0
NamrathaShekar212 <namrathashekarc@gmail.com>
2025-07-25 15:05:35 +0530

    Create variables.py
    implement variable declarations and type display in variables.py

---
+++ Variable/variables.py
@@ -0,0 +1,10 @@
+name = "Namratha C"
+age = 23
+height = 5.12
+is_student = True
+print(f"{name} is {age} years old and her height is {height feet."}.
+print(f"Is she a student of python? {'Yes' if is_student else 'No'}")
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
